f621b762af55edae6b20e57a51f1578339a63f3b
AlfonsoAtreides <88739339+A01641937-alf@users.noreply.github.com>
2025-04-29 22:19:14 -0600

    Create example.c

---
+++ example.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    float y = 20.5;
+    char c = 'A';
+
+    if (x < y) {
+        printf("x value is lower than y");
+    } else {
+        printf("x value is higher more than y");
+    }
+
+    //Hellow
+    /*hola
+      si funciona*/
+
+    return 0;
+}
